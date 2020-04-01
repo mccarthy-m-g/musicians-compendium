c7ed263c92d17ccac627e357fa9cd00638eb9836
Michael McCarthy <51542091+mccarthy-m-g@users.noreply.github.com>
2020-04-01 16:05:20 -0700

    change embed_audio() argument name

--- _common.R
+++ _common.R
@@ -28,13 +28,13 @@ options(dplyr.print_min = 6,
 ###############################################################################
 
 embed_audio <- function(src, type = "audio/mpeg", attribute = "controls",
-                        unsupported = "") {
+                        placeholder = "") {
 
   if (knitr::is_html_output()) {
     sprintf("<audio %3$s>
              <source src='%1$s' type='%2$s'>
           </audio>", src, type, attribute)
-  } else print(unsupported)
+  } else print(placeholder)
     
 }
 
